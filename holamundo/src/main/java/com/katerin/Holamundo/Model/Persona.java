5fa1519f7d366a7a61fe02a76e4c063fe38572a9
katherinc <katherinalarcon7006@gmail.com>
2020-06-11 10:52:04 -0600

    actualizacion

---
+++ holamundo/src/main/java/com/katerin/Holamundo/Model/Persona.java
@@ -0,0 +1,40 @@
+package com.katerin.Holamundo.Model;
+
+public class Persona {
+private String Nombre;
+public String getNombre() {
+	return Nombre;
+}
+public void setNombre(String nombre) {
+	Nombre = nombre;
+}
+public String getApellido() {
+	return Apellido;
+}
+public void setApellido(String apellido) {
+	Apellido = apellido;
+}
+public String getTelefono() {
+	return telefono;
+}
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+private String Apellido;
+private String telefono;
+private String email;
+
+
+
+
+
+
+
+
+}
--- holamundo/src/main/java/com/katerin/Holamundo/holamundo/Controlador.java
+++ holamundo/src/main/java/com/katerin/Holamundo/holamundo/Controlador.java
@@ -1,8 +1,13 @@
 package com.katerin.Holamundo.holamundo;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.katerin.Holamundo.Model.Persona;
 
 
 @Controller
@@ -10,9 +15,30 @@ public class Controlador {
 	@GetMapping("/")
 	
 	public String Inicio(Model modelo) {
+		Persona person = new Persona();
+		person.setNombre("Katerin");
+		person.setApellido("Alarcon");
+		person.setTelefono("78906547");
+		person.setEmail("katyalarcon7006@gmail.com");
+		
+		Persona person2 = new Persona();
+		person2.setNombre("Katy");
+		person2.setApellido("Alarcon");
+		person2.setTelefono("7890654799");
+		person2.setEmail("katyalarcon90@gmail.com");
+		
+		List personitas = Arrays.asList(person,person2);
+		
+		
+		
+		
+		
 		String mensaje = "HOLA DESDE EL CONTROLADOR";
 		modelo.addAttribute("saludo", mensaje);
-			return "index";
+		//modelo.addAttribute("persona",person);
+		modelo.addAttribute("persona",personitas);
+		
+		return "index";
 			
 	}
 	
